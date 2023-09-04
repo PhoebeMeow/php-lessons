10d247a3eb78bbf2f9d08458809a63615a771a2e
PhoebeMeow <302303913@student.rocmondriaan.nl>
2023-09-04 15:17:06 +0200

    opdracht 1 - 2 af
    i finished a lil bit but that's okay.

--- h4.php
+++ h4.php
@@ -1,1 +1,25 @@
 <?php
+
+//opdracht 1
+    $hour = date("G");
+
+    if ( date("G") >= 6 && date("G") < 12 ) {
+        echo "Het is ochtend";
+    } else if (date("G") >= 12 && date("G") < 18) {
+        echo "Het is middag";
+    } else if (date("G") >= 18 && date("G") < 24) {
+        echo "Het is avond";
+    } else if (date("G") >= 24 && date("G") < 6) {
+        echo "Het is nacht";
+    }
+
+    //opdracht 2
+    $time = match($hour) {
+        "6", "7", "8", "9", "10", "11", "12" => "Het is ochtend",
+        "12", "13", "14", "15", "16", "17", "18" => "Het is middag",
+        "18", "19", "20", "21", "22", "23", "24" => "Het is avond",
+        "24", "0", "1", "2", "3", "4", "5", "6" => "Het is nacht",
+    };
+    echo "<br> $time";
+
+    //opdracht 3
