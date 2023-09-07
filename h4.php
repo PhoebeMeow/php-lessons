6a3926e7da8fc28f86ca8b2a8c940281f7eb7531
LitlMeow <302303913@student.rocmondriaan.nl>
2023-09-07 20:37:52 +0200

    opdracht 4-7
    I finished opdracht 4-6 but I made a start on opdracht 7 :)

--- h4.php
+++ h4.php
@@ -33,3 +33,46 @@
         $result = $x * 2 + $y;
         echo "<br> Uitkomst is: $result";
     }
+
+    //opdracht 4
+    $oldPrice = 200;
+    $cheapPrice = 40;
+
+    if ($oldPrice >= 150 ) {
+        $newPrice = 150 / 100 * 19 + $oldPrice;
+        echo "<br> Oude prijs: €$oldPrice. Na verhoging van 19% is de prijs: €$newPrice";
+    }
+
+    if ($cheapPrice <= 55) {
+        $expensivePrice = 55 / 100 * 11 + $cheapPrice;
+        echo "<br> Oude prijs: €$cheapPrice. Na verhoging van 11% is de prijs: €$expensivePrice";
+    }
+
+    //opdracht 5
+    $number = 48;
+
+    $numberResult = match($number) {
+        2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48 => "even",
+        1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49 => "oneven",
+    };
+    echo "<br> Het getal $number is $numberResult";
+
+
+    $uur = date("G");
+    $temperature = 25;
+    $humidity = 50;
+
+    if (date("G") >= 8 && date("G") <= 17) {
+        echo "<br> De airco moet aan";
+    } else if ($temperature > 20 && $humidity > 85) {
+        echo "<br> De airco moet aan";
+    } else if (date("G") >= 17) {
+        echo "<br> De airco moet uit";
+    } else if ($temperature < 20 && $humidity < 85) {
+        echo "<br> De airco moet uit";
+    }
+
+    //opdracht 7
+    $spaargeld = 250;
+    $iphone = 1000;
+    if ($spaargeld < $iphone)
