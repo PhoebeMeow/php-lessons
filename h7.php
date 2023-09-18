8125e5ed8a6cdcefa82712c158d60bf449661616
PhoebeMeow <302303913@student.rocmondriaan.nl>
2023-09-18 15:21:46 +0200

    stukje van opdracht 7 gedaan
    een klein begin gemaakt aan opdracht 7

--- h7.php
+++ h7.php
@@ -1,5 +1,13 @@
 <?php
     //opdracht 1
-
+    if (isset($_POST['uitrekenen'])) {
+        if ($_POST('btw') == 6) {
+            $melding = "Bedrag inclusief 6% BTW: € " .
+                ($_POST ['bedrag'] * 1.06);
+        } else {
+            $melding = "Bedrag inclusief 21 BTW: € " .
+                ($_POST['bedrag'] * 1.21);
+        }
+    }
 
 
