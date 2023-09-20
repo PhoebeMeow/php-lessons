3328674b121759a5437b78fbf6d5255ebec13e5d
PhoebeMeow <302303913@student.rocmondriaan.nl>
2023-09-20 14:24:44 +0430

    opdracht 1 h7 af
    heb opdracht 1 van h7 af

--- h7.php
+++ h7.php
@@ -9,5 +9,27 @@
                 ($_POST['bedrag'] * 1.21);
         }
     }
+    ?>
+
+<form method="post" action="">
+
+    <label>Bedrag exclusief BTW</label>
+    <input type="text" name="bedrag">
+
+    <br>
+
+    <input class="nostyle" type="radio" name="btw" value="6"> Laag, 6%
+
+    <br>
+
+    <input class="nostyle" type="radio" name="btw" value="21"> Hoog, 21%
+
+    <br><br>
+    <input type="submit" name="uitrekenen" value="Uitrekenen">
+</form>
+
+<br><br>
+
+<?php echo $melding; ?>
 
 
