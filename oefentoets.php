a962bd300fba978ecdf35aedb752f32b0b02f1ad
PhoebeMeow <302303913@student.rocmondriaan.nl>
2023-09-25 15:22:50 +0200

    oefentoets
    Ik heb opdracht 1 ervan bijna af :D

---
+++ oefentoets.php
@@ -0,0 +1,37 @@
+<form method="post" action="">
+
+    <label>Stappelkorting</label>
+
+    <br>
+
+    <input class="nostyle" type="radio" name="keuze" value="22" required> Handdoek
+
+    <br>
+
+    <input class="nostyle" type="radio" name="keuze" value="17" required> Broek
+
+    <br>
+
+    <input class="nostyle" type="radio" name="keuze" value="10" required> Shirt
+
+    <br><br>
+
+    Amount: <input class="nonstyle" type="text" name="amount" required>
+
+    <br><br>
+
+    <input type="submit" name="uitrekenen" value="Uitrekenen">
+</form>
+
+<?php
+//opdracht 1
+    if(isset ($_POST['uitrekenen'])) {
+        $amount = filter_input(INPUT_POST, 'amount', FILTER_VALIDATE_INT);
+
+        if(!$amount) {
+            echo "Voer een getal in";
+        }
+    }
+
+
+?>
